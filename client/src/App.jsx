2d63bede988c0b0c85e00adde72b52a4525520b8
rwiteshbera <rwiteshbera@gmail.com>
2022-05-10 01:08:01 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <div className="main-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
