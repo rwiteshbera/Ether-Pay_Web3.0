e979180d6c6195de0415a6ef63964640867a050f
rwiteshbera <rwiteshbera@gmail.com>
2022-05-10 17:50:20 +0530

    Updated

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,17 +5,31 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
 
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const Input = ({ placeholder, name, type, value, handleChnage }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChnage(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
 
+const handleSubmit = () => {
+
+}
 
 const Welcome = () => {
   const connectWallet = () => {};
 
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between mf:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-2">
             Send Crypto <br />
             across the world
@@ -38,6 +52,61 @@ const Welcome = () => {
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web3.0</div>
             <div className={`${commonStyles}`}>Low Fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-sm mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-5 sm:w-96 flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChnage={() => {}}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChnage={() => {}}
+            />
+            <Input
+              placeholder="Keyword (GIF)"
+              name="keyword"
+              type="text"
+              handleChnage={() => {}}
+            />
+            <Input
+              placeholder="Enter message"
+              name="message"
+              type="text"
+              handleChnage={() => {}}
+            />
+
+            <div className="h-[1px] w-full bg-gray-400 my-2"/>
+
+            {false ? (
+                <Loader />
+            ) : (
+                <button type="button" onClick={handleSubmit}
+                 className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">Pay Now</button>
+            )}
           </div>
         </div>
       </div>
